da38ea819e20ea3c76f64b6cdbaf8a95e0b4a9d6
Nick Topacio <ntopacio25@gmail.com>
2023-10-12 23:02:01 -0400

    Add card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,11 +4,25 @@ function Card() {
   return (
     <>
       <div className="card">
+        <div className="card--status">
+          <p>SOLD OUT</p>
+        </div>
         <img
           src="src/assets/katie-zaferes.png"
-          alt=""
+          alt="img of katie zaferes"
           className="card--image"
         />
+        <div className="card--subtitle">
+          <div className="card--rating_and_reviews">
+            <img className="card--star" src="src/assets/star.png" alt="star" />
+            <p className="card--rating">5.0</p>
+            <p className="card--review_and_location">(6)•USA</p>
+          </div>
+          <p className="card--desc">Life lessons with Katie Zaferes</p>
+          <p className="card--price">
+            <strong>From $136</strong> / person
+          </p>
+        </div>
       </div>
     </>
   );
