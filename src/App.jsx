b970bf8bf27cbda2f71e50ce2765bfea5a1fbc2a
Nick Topacio <topacio.3@osu.edu>
2023-10-22 19:06:40 -0400

    Add props to card component and parsing of data.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
 import Card from "./components/Card.jsx";
+import data from "./data.js";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
-      <Card />
+      {data.map((item) => {
+        return (
+          <Card
+            image={item.coverImg}
+            rating={item.stats.rating}
+            reviewCount={item.stats.reviewCount}
+            location={item.location}
+            title={item.title}
+            price={item.price}
+          />
+        );
+      })}
     </>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,20 @@
 import "./Card.css";
 
-function Card() {
+function Card(props) {
   return (
     <>
       <div className="card">
-        <img
-          src="src/assets/katie-zaferes.png"
-          alt=""
-          className="card--image"
-        />
+        <img src={`src/assets/${props.image}`} alt="" className="card--image" />
+        <div className="card--stats">
+          <img src="src/assets/star.png" alt="star" />
+          <span>{props.rating}</span>
+          <span className="fade">({props.reviewCount}) • </span>
+          <span className="fade">{props.location}</span>
+        </div>
+        <p>{props.title}</p>
+        <p>
+          <span className="bold">From ${props.price}</span> / person
+        </p>
       </div>
     </>
   );
